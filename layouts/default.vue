9bfdfbeaa9b2f4ba3696cdbe66bad8a385c17a29
Savan Cvijetic <Savan.Cvijetic@outlook.com>
2021-06-04 13:47:42 +0200

    englisch to german

--- layouts/default.vue
+++ layouts/default.vue
@@ -92,17 +92,17 @@
                           href="#"
                           class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
                           role="menuitem"
-                        >Your Profile</a>
+                        >Dein Profil</a>
                         <a
                           href="#"
                           class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
                           role="menuitem"
-                        >Settings</a>
+                        >Einstellungen</a>
                         <a
                           href="#"
                           class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
                           role="menuitem"
-                        >Sign out</a>
+                        >Abmelden</a>
                       </div>
                     </div>
                   </transition>
@@ -186,15 +186,15 @@
               <a
                 href="#"
                 class="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
-              >Your Profile</a>
+              >Dein Profil</a>
               <a
                 href="#"
                 class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
-              >Settings</a>
+              >Einstellungen</a>
               <a
                 href="#"
                 class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
-              >Sign out</a>
+              >Abmelden</a>
             </div>
           </div>
         </div>
